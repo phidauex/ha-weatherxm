fb33fe18150d4bf4f98ccb9f23a527faffee985b
Sam <sam.ley@gmail.com>
2024-10-25 22:28:45 -0600

    Add default forecast constants

--- custom_components/weatherxm/const.py
+++ custom_components/weatherxm/const.py
@@ -1,5 +1,7 @@
 """Constants for the WeatherXM integration."""
 
 DOMAIN = "weatherxm"
+DEFAULT_FORECAST_DAYS = 7
+DEFAULT_FORECAST_HOURS = 48
 DEFAULT_HOST = "https://api.weatherxm.com"
 CONF_FILTER_OWNED_DEVICES = "filter_owned"
